c601df056752d6554e38cbd3a88ce4382abc14e7
Cory Sabol <corygrandhamsabol@gmail.com>
2017-10-22 05:33:39 -0400

    fixed syntax error in sql, container now builds

--- setup_cmds.sql
+++ setup_cmds.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE pathway-db;
+CREATE DATABASE pathwaydb;
 /*Make sure to update this for production
  to be actually secure.*/
-CREATE django WITH PASSWORD 'password';
+CREATE USER django WITH PASSWORD 'password';
 
 ALTER ROLE django SET client_encoding TO 'utf8';
 ALTER ROLE django SET default_transaction_isolation TO 'read committed';
 ALTER ROLE django SET timezone TO 'UTC';
 
-GRANT ALL PRIVILEGES ON DATABASE pathway-db TO django;
+GRANT ALL PRIVILEGES ON DATABASE pathwaydb TO django;
