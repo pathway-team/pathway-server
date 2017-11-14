eb83a8a98366575b72a2b3c4acc1089a04ecfe64
Cory Sabol <corygrandhamsabol@gmail.com>
2017-11-13 21:46:14 -0500

    added web as allowed host

--- pathwayserv/settings.py
+++ pathwayserv/settings.py
@@ -23,16 +23,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8fmp^*+c_p(x7$1^@o_q!%@_1qrdu%u^qlb_7$465$xdfzf@5&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = False
+DEBUG = True
+#DEBUG = False
 
-'''
 ALLOWED_HOSTS = [
         '127.0.0.1',
-        '138.197.103.225'
+        '138.197.103.225',
+        'web'
         ]
-'''
-ALLOWED_HOSTS = []
 
 # Application definition
 
